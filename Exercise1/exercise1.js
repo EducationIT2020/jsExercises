ff228872f057841d39d95a434a391e70c296f853
PatricioSasso <patonsasso@gmail.com>
2020-09-22 19:51:18 -0300

    Done exercise 1

---
+++ Exercise1/exercise1.js
@@ -0,0 +1,10 @@
+// 1. En base el numero ingresado calcular si una persona puede conducir?
+var edad = prompt("Ingrese su edad");
+if(edad > 17)
+{
+    alert("Puede conducir");
+}
+else
+{
+    alert("No puede conducir");
+}
